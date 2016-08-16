1471ae0b5b759a34ec3938f417cd2bfb85bcadf2
qeouo <onono36@hotmail.com>
2016-08-16 11:22:35 +0900

    add

--- test3d/test3d.js
+++ test3d/test3d.js
@@ -385,7 +385,6 @@ var Test3d=(function(){
 		
 	}
 	ret.start = function(){
-	alert("start");
 		sky = Util.loadImage("sky.png",1);
 		Util.loadImage("sky.jpg",1,function(image){
 			gl.bindTexture(gl.TEXTURE_2D, image.gltexture);
