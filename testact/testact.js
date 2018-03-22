2c6c2002b7fe05476fd06ec460ccf8154f650905
qeouo <qeououo@gmail.com>
2018-03-22 17:38:27 +0900

    softbody

--- testact/testact.js
+++ testact/testact.js
@@ -178,44 +178,44 @@ var Testact=(function(){
 
 			ono3d.rf=0;
 			if(obj3d){
-				//if(obj3d.scenes.length>0){
-				//	var objects = obj3d.scenes[globalParam.scene].objects;
-				//	for(var i=0;i<objects.length;i++){
-				//		if(objects[i].hide_render){
-				//			continue;
-				//		}
-				//		ono3d.lineWidth=1;
-				//		ono3d.rf&=~Ono3d.RF_OUTLINE;
-				//		if(globalParam.outlineWidth>0.){
-				//			ono3d.lineWidth=globalParam.outlineWidth;
-				//			ono3d.rf|=Ono3d.RF_OUTLINE;
-				//			Util.hex2rgb(ono3d.lineColor,globalParam.outlineColor);
-				//		}
-				//		if(bane){
-				//			if(bane.con2.name == objects[i].name){
-				//				ono3d.lineWidth=1;
-				//				ono3d.rf|=Ono3d.RF_OUTLINE;
-				//				Vec4.set(ono3d.lineColor,1,4,1,0);
-				//			}
-				//		}
-				//		if(globalParam.physics){
-				//			O3o.drawObject(objects[i],phyObjs);
-				//		}else{
-				//			O3o.drawObject(objects[i],null);
-				//		}
-				//	}
-				//}
-				var t = obj3d.objectsN["アーマチュア"].poseArmature;
-				var s = poseArmature;
-				s.reset();
-				s.setAction(obj3d.actions[2],timer/1000.0*24);
-				t.setAction(obj3d.actions[1],timer/1000.0*24);
-				O3o.PoseArmature.sub(s,s,t);
-				O3o.PoseArmature.mul(s,s,0.2);
-				O3o.PoseArmature.add(t,s,t);
-
-
-				O3o.drawObject(obj3d.objectsN["human"]);
+				if(obj3d.scenes.length>0){
+					var objects = obj3d.scenes[globalParam.scene].objects;
+					for(var i=0;i<objects.length;i++){
+						if(objects[i].hide_render){
+							continue;
+						}
+						ono3d.lineWidth=1;
+						ono3d.rf&=~Ono3d.RF_OUTLINE;
+						if(globalParam.outlineWidth>0.){
+							ono3d.lineWidth=globalParam.outlineWidth;
+							ono3d.rf|=Ono3d.RF_OUTLINE;
+							Util.hex2rgb(ono3d.lineColor,globalParam.outlineColor);
+						}
+						if(bane){
+							if(bane.con2.name == objects[i].name){
+								ono3d.lineWidth=1;
+								ono3d.rf|=Ono3d.RF_OUTLINE;
+								Vec4.set(ono3d.lineColor,1,4,1,0);
+							}
+						}
+						if(globalParam.physics){
+							O3o.drawObject(objects[i],phyObjs);
+						}else{
+							O3o.drawObject(objects[i],null);
+						}
+					}
+				}
+//				var t = obj3d.objectsN["アーマチュア"].poseArmature;
+//				var s = poseArmature;
+//				s.reset();
+//				s.setAction(obj3d.actions[2],timer/1000.0*24);
+//				t.setAction(obj3d.actions[1],timer/1000.0*24);
+//				O3o.PoseArmature.sub(s,s,t);
+//				O3o.PoseArmature.mul(s,s,0.2);
+//				O3o.PoseArmature.add(t,s,t);
+//
+//
+//				O3o.drawObject(obj3d.objectsN["human"]);
 			}
 			break;
 		}
